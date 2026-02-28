3c76102e76c7738c6aa7fc8bef27a641e017727d
melik-droid <melikcaymaz@gmail.com>
2026-02-28 15:18:15 +0300

    changed rainbowkit modal

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import { monadTestnet } from "./config/monad";
 import App from "./App";
 import "@rainbow-me/rainbowkit/styles.css";
@@ -16,11 +16,45 @@ const wagmiConfig = createConfig({
 
 const queryClient = new QueryClient();
 
+const obscuraTheme = darkTheme({
+  accentColor: "#2081e2",
+  accentColorForeground: "white",
+  borderRadius: "small",
+  fontStack: "system",
+  overlayBlur: "small",
+});
+
+// Override specific modal styles to match Obscura
+obscuraTheme.colors.connectButtonBackground = "transparent";
+obscuraTheme.colors.modalBackground = "#0a0a0f";
+obscuraTheme.colors.modalBorder = "#1a1a2e";
+obscuraTheme.colors.profileForeground = "#0d0d14";
+obscuraTheme.colors.actionButtonBorder = "#1a1a2e";
+obscuraTheme.colors.actionButtonSecondaryBackground = "#12121f";
+obscuraTheme.colors.menuItemBackground = "#12121f";
+obscuraTheme.fonts.body = "'JetBrains Mono', monospace";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
+        <RainbowKitProvider
+          theme={obscuraTheme}
+          appInfo={{
+            appName: "Obscura",
+            learnMoreUrl:
+              "https://github.com/GTU-Blockchain/defake-monad-blitz-istanbul",
+          }}
+          avatar={({ size }) =>
+            React.createElement("img", {
+              src: "/logo.svg",
+              alt: "Obscura",
+              width: size,
+              height: size,
+              style: { borderRadius: 4 },
+            })
+          }
+        >
           <BrowserRouter>
             <App />
           </BrowserRouter>
